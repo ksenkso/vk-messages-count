5fc4c217adb987998efeba68daed0eca561abb0f
ksenkso <yazunya@gmail.com>
2017-07-30 02:19:16 +0300

    Test auth dialog

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const {URLSearchParams} = require('url');
+const {request} = require('https');
 const process = require('process');
 const express = require('express');
 const router = express.Router();
@@ -27,7 +28,50 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/oauth', (req, res, next) => {
-  res.render('auth');
+    const { code } = req.params;
+
+    const atURL = 'https://oauth.vk.com/access_token/?';
+    const params = new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: process.env.REDIRECT_URI,
+        client_secret: process.env.CLIENT_SEECRET,
+        code,
+
+    });
+
+    request({
+        hostname: 'oauth.vk.com',
+        protocol: 'https',
+        method: 'GET',
+        path: '/access_token/?' + params.toString()
+    }, response => {
+
+        let responseText = '';
+
+        response.on('data', data => {
+            responseText += data;
+        });
+
+        response.on('end', () => {
+            try {
+                const jsonData = JSON.parse(responseText);
+
+                if (jsonData.access_token) {
+                    req.session.access_token = jsonData.access_token;
+                    req.session.expires_in = jsonData.expires_in;
+                    req.session.user_id = jsonData.user_id;
+
+                    res.redirect('auth', {id: req.session.user_id});
+                }
+            } catch (e) {
+                res.render('error', e);
+            }
+        })
+    });
+
+
+
+    res.render('auth', {code});
 });
 
 module.exports = router;
